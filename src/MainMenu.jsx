04f78033ad0e98bd78951dec00f1e68e516aec3f
RosaliaCampusano <rosalia.campusano@ikasle.aeg.eus>
2025-09-17 13:55:25 +0200

    feat: insert the logic to implement the button to display the music

--- src/MainMenu.jsx
+++ src/MainMenu.jsx
@@ -1,12 +1,28 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import mainMenuMusic from "./assets/sounds/main-menu.mp3";
+import useSound from "use-sound";
 
 function MainMenu({ setState }) {
+  const [isMusicPlaying, setIsMusicPlaying] = useState(false);
+  const [play, { stop }] = useSound(mainMenuMusic, { volume: 0.25 });
+
+  useEffect(() => {
+    isMusicPlaying ? stop() : play();
+  }, [isMusicPlaying]);
+
   return (
     <>
       <div className="main-menu">
         <div className="main-menu-container">
           <h1>The Elixir Trials of Simon</h1>
           <h2>Knock the door and fate awaits inside the inn</h2>
+          <button
+            className="button-menu-music"
+            onClick={() => {
+              setIsMusicPlaying(!isMusicPlaying);
+            }}
+          ></button>
           <div className="fire-animation"></div>
         </div>
         <button
