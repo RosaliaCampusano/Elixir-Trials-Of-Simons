a3efbe60d284fc5dabf7983b8b347a027d2a0fe1
Rosalia Campusano <rosalia.campusano@ikasle.aeg.eus>
2025-09-17 23:21:46 +0200

    refactor: move the music logic into the App to control it

--- src/MainMenu.jsx
+++ src/MainMenu.jsx
@@ -1,34 +1,18 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-import mainMenuMusic from "./assets/sounds/main-menu.mp3";
-import useSound from "use-sound";
 
-function MainMenu({ setState }) {
-  const [isMusicPlaying, setIsMusicPlaying] = useState(false);
-  const [play, { stop }] = useSound(mainMenuMusic, { volume: 0.25 });
-
-  useEffect(() => {
-    isMusicPlaying ? stop() : play();
-  }, [isMusicPlaying]);
-
+function MainMenu({ startGame }) {
   return (
     <>
       <div className="main-menu">
         <div className="main-menu-container">
           <h1>The Elixir Trials of Simon</h1>
           <h2>Knock the door and fate awaits inside the inn</h2>
-          <button
-            className="button-menu-music"
-            onClick={() => {
-              setIsMusicPlaying(!isMusicPlaying);
-            }}
-          ></button>
           <div className="fire-animation"></div>
         </div>
         <button
           className="door-button"
           onClick={() => {
-            setState(1);
+            startGame();
           }}
         ></button>
       </div>
