5c72008df68e56749475dac072ad6b9ad3bc6d13
RosaliaCampusano <rosalia.campusano@ikasle.aeg.eus>
2025-09-18 11:41:25 +0200

    feat: implement the logic for the music and inject each component with its props

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,31 @@
 import "./App.css";
 import Game from "./Game";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import useSound from "use-sound";
 import MainMenu from "./MainMenu";
-import mainMenuMusic from "./assets/sounds/main-menu.mp3";
 import gameMusic from "./assets/sounds/game-music.mp3";
+import mainMenuMusic from "./assets/sounds/main-menu.mp3";
 import talkingSound from "./assets/sounds/sounds_talking.mp3";
 
 function App() {
   const [state, setState] = useState(0);
+  const [isMusicPlaying, setIsMusicPlaying] = useState(false);
   // === Background sounds ===
-  const [
-    playMainMenuMusic,
-    { stop: stopMainMenuMusic, isPlaying: isMainMenuPlaying },
-  ] = useSound(mainMenuMusic, { volume: 0.25, loop: true });
-  const [
-    playGameMusic,
-    { pause: pauseGameMusic, isPlaying: isGameMusicPlaying },
-  ] = useSound(gameMusic, { volume: 0.2, loop: true });
-  const [
-    playTalkingSound,
-    { pause: pauseTalkingSound, isPlaying: isTalkingPlaying },
-  ] = useSound(talkingSound, { volume: 0.1, loop: true });
+  const [playMainMenuMusic, { stop: stopMainMenuMusic }] = useSound(
+    mainMenuMusic,
+    {
+      volume: 0.2,
+      loop: true,
+    }
+  );
+  const [playGameMusic, { pause: pauseGameMusic }] = useSound(gameMusic, {
+    volume: 0.2,
+    loop: true,
+  });
+  const [playTalkingSound, { pause: pauseTalkingSound }] = useSound(
+    talkingSound,
+    { volume: 0.1, loop: true }
+  );
 
   const startGame = () => {
     stopMainMenuMusic();
@@ -30,16 +34,16 @@ function App() {
     setState(1);
   };
 
-  useEffect(() => {
-    if (state === 0 && !isMainMenuPlaying) {
-      playMainMenuMusic();
-    }
-  }, [state, playMainMenuMusic, isMainMenuPlaying]);
-
   return (
     <>
       {state === 0 ? (
-        <MainMenu startGame={startGame} />
+        <MainMenu
+          startGame={startGame}
+          playMainMenuMusic={playMainMenuMusic}
+          stopMainMenuMusic={stopMainMenuMusic}
+          isMusicPlaying={isMusicPlaying}
+          setIsMusicPlaying={setIsMusicPlaying}
+        />
       ) : (
         <Game
           state={state}
@@ -48,6 +52,8 @@ function App() {
           pauseGameMusic={pauseGameMusic}
           playTalkingSound={playTalkingSound}
           pauseTalkingSound={pauseTalkingSound}
+          playMainMenuMusic={playMainMenuMusic}
+          setMainMenuMusicIsPlaying={setIsMusicPlaying}
         />
       )}
     </>
