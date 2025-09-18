bee7f26d59e93cede6dd910f0a316bf2aa71989f
RosaliaCampusano <rosalia.campusano@ikasle.aeg.eus>
2025-09-18 11:44:13 +0200

    feat: insert the music and insert the logic to change the button if the music is playing or not

--- src/MainMenu.jsx
+++ src/MainMenu.jsx
@@ -1,9 +1,37 @@
+import soundButton from "./assets/images/sound-button.png";
+import pauseButton from "./assets/images/pause-button.png";
+
 import "./App.css";
 
-function MainMenu({ startGame }) {
+function MainMenu({
+  startGame,
+  playMainMenuMusic,
+  stopMainMenuMusic,
+  isMusicPlaying,
+  setIsMusicPlaying,
+}) {
+  const toggleMusic = () => {
+    if (isMusicPlaying) {
+      stopMainMenuMusic();
+      setIsMusicPlaying(false);
+    } else {
+      playMainMenuMusic();
+      setIsMusicPlaying(true);
+    }
+  };
+
   return (
     <>
       <div className="main-menu">
+        <button
+          className="music-button"
+          onClick={toggleMusic}
+          style={{
+            backgroundImage: isMusicPlaying
+              ? `url(${soundButton})`
+              : `url(${pauseButton})`,
+          }}
+        ></button>
         <div className="main-menu-container">
           <h1>The Elixir Trials of Simon</h1>
           <h2>Knock the door and fate awaits inside the inn</h2>
